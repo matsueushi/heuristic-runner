19e1023d68b56742fc4aad34e6136d87e584e4cb
matsueushi <matsueushi@gmail.com>
2023-05-04 21:07:03 +0900

    header

--- runner/src/App.tsx
+++ runner/src/App.tsx
@@ -1,13 +1,10 @@
-import "./App.css";
 import Header from "./Header";
-import TestCaseTable from "./Table";
 import Invoker from "./Invoker";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <TestCaseTable />
       <Invoker />
     </div>
   );

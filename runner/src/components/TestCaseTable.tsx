7280c4e040dbbf70fc6bf7d957fadc5164533558
matsueushi <matsueushi@gmail.com>
2023-05-11 00:08:33 +0900

    improve table

--- runner/src/components/TestCaseTable.tsx
+++ runner/src/components/TestCaseTable.tsx
@@ -1,30 +1,47 @@
-import { Table, TableContainer, TableBody, Paper } from "@mui/material";
+import { useMemo } from "react";
+import MaterialReactTable, { MRT_ColumnDef } from "material-react-table";
 
+import { formatLongText } from "../services/utility";
 import { TestCase } from "./TestCase";
-import TestCaseTableRow from "./TestCaseTableRow";
-import TestCaseTableHead from "./TestCaseTableHead";
 
 interface TestCaseTableProps {
   testCases: TestCase[];
 }
 
 function TestCaseTable({ testCases }: TestCaseTableProps) {
-  return (
-    <TableContainer component={Paper}>
-      <Table
-        sx={{ minWidth: 650 }}
-        size="small"
-        aria-label="the test case table"
-      >
-        <TestCaseTableHead />
-        <TableBody>
-          {testCases.map((testCase) => (
-            <TestCaseTableRow key={testCase.seed} testCase={testCase} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+  const columns = useMemo<MRT_ColumnDef<TestCase>[]>(
+    () => [
+      {
+        accessorKey: "seed",
+        header: "Seed",
+      },
+      {
+        accessorKey: "score",
+        header: "Score",
+      },
+      {
+        accessorKey: "baseScore",
+        header: "BaseScore",
+      },
+      {
+        accessorFn: (row) => row.score - row.baseScore,
+        header: "Diff",
+      },
+      {
+        accessorFn: (row) => formatLongText(row.input),
+        enableClickToCopy: true,
+        header: "Input",
+      },
+      {
+        accessorFn: (row) => formatLongText(row.output),
+        enableClickToCopy: true,
+        header: "Output",
+      },
+    ],
+    []
   );
+
+  return <MaterialReactTable columns={columns} data={testCases} />;
 }
 
 export default TestCaseTable;
